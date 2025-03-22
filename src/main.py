a4127c9669b564ea89b2800cdc2edab26eca12cc
Andres Navarro Pedregal <me@andresnav.com>
2025-03-22 13:03:18 +0100

    chore: add another test

--- src/main.py
+++ src/main.py
@@ -7,7 +7,8 @@ from ocr import AWSRekognitionOCR
 from doc import FormDoc
 
 DIR = os.path.dirname(os.path.realpath(__file__))
-FORM_PATH = os.path.join(DIR, "../docs/alta_autonomos.pdf")
+# FORM_PATH = os.path.join(DIR, "../docs/alta_autonomos.pdf")
+FORM_PATH = os.path.join(DIR, "../docs/consulta_de_fondos.pdf")
 PIC_PATH = os.path.join(DIR, "../docs/dni.jpg")
 
 load_dotenv()
@@ -34,6 +35,15 @@ extracted_text = awsRekognitionOCR.process_image(PIC_PATH)
 
 prompt = f"""
     Dado el texto obtenido abajo, quiero que rellenes los siguientes campos {fields_to_fill}.
+
+    Tambien te doy mas informacion:
+    - telefono 666666666
+    - correo electronico: test@example.com
+    - seccion: test 1
+    - caja/legajo: test 1
+    - expediente: test 1
+
+    Hoy es 22 de marzo de 25
 
     Formato: el resultado tiene que ser un JSON como en el ejemplo con los campos rellenados, nada más. No me des nada más que no sea el JSON con los campos rellenados.
 
@@ -64,6 +74,6 @@ output_dir = os.path.join(DIR, "../docs")
 os.makedirs(output_dir, exist_ok=True)
 
 # Save the filled PDF
-output_path = os.path.join(output_dir, "filled_form.pdf")
+output_path = os.path.join(output_dir, "consulta_de_fondos_filled.pdf")
 formDoc.save(output_path)
 print(f"\nSuccessfully saved filled form to:\n{os.path.abspath(output_path)}")
